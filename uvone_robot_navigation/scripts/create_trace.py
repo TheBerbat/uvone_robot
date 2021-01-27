01ab5b919416f2e105aa105a9696b5b5d83f6d10
Rafael Martin <theverbat@gmail.com>
2021-01-27 22:55:42 +0100

    fixed create_trace.py

--- uvone_robot_navigation/scripts/create_trace.py
+++ uvone_robot_navigation/scripts/create_trace.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python3
+import rospy
 import cv2
 import math
 import yaml
 
 origen = [0, 0, 0]
 grid_size = 0
+path = ''
+#path_yaml = '/home/rafaelm/Desktop/mapa_test.yaml'
+path_yaml = '/home/rafaelm/own_ws/src/uvone_robot/uvone_robot_navigation/maps/octomap_grid.yaml'
+f = open("demofile2.txt", "w") # Archivo donde se guardaran las posiciones objetivo
 
-f = open("demofile2.txt", "w")
-
-with open('/home/rafaelm/Desktop/mapa_test.yaml') as file:
-
-    documents = yaml.load(file, Loader=yaml.FullLoader)
-
-    for item, doc in documents.items():
-
-        if item == 'origin':
-            origen = doc
-
-        if item == 'resolution':
-            grid_size = doc
-
-        if item == 'image':
-            path = doc
+height = 0
+width = 0
+channels = 0
 
 
-img = cv2.imread(path)
-height, width, channels = img.shape
 
 init_point = (0,0)
 final_point = (0,0)
 init_point_off = (0,0)
 final_point_off = (0,0)
+
 
 def on_click(event, x, y, p1, p2):
     global init_point, final_point, init_point_off, final_point_off, height, width
@@ -57,7 +48,7 @@ def on_click(event, x, y, p1, p2):
         final_point = (int(init_point[0] + vector[0]), int(init_point[1] + vector[1]))
 
 
-        f.write("%.4f  \t%.4f  \t%.4f\n" % (init_point_off[0], init_point_off[1], 1))
+        f.write("%.4f  \t%.4f  \t%.4f\n" % (init_point_off[0], init_point_off[1], math.atan2(vector[1],vector[0])))
 
 
         cv2.arrowedLine(img, init_point, final_point, (100, 200, 0), 5)
@@ -66,17 +57,54 @@ def on_click(event, x, y, p1, p2):
 
 
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', on_click)
-cv2.imshow("image", img)
 
-while(1):
-    k = cv2.waitKey(33)
-    if k==32:    # Esc key to stop
-        break
-    else:  # normally -1 returned,so don't print it
-        continue
-  
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    
+    f = open("demofile2.txt", "w")
+    
+    try:
 
-f.close()
+        # Obtenemos la informacion del yaml del mapa y lo agregamos a las variables globales.
+
+        with open(path_yaml) as file:
+
+            datos = yaml.safe_load(file)
+
+            for item, doc in datos.items():
+
+                if item == 'origin':
+                    origen = doc
+
+                if item == 'resolution':
+                    grid_size = doc
+
+                if item == 'image':
+                    path = doc
+
+
+        # Leemos el archivo del mapa, y medimos algunas propiedades.
+
+        img = cv2.imread(path)
+        height, width, channels = img.shape
+
+        # Mostramos el mapa y anadimos el callback.
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image', on_click)
+        cv2.imshow("image", img)
+
+
+        # Esperamos que el usuario presione entres para salir y escribir el archivo.
+        while(1):
+            k = cv2.waitKey(33)
+            if k==32:
+                break
+            else:
+                continue
+        
+        cv2.destroyAllWindows()
+        f.close()
+
+        pass
+
+    except rospy.ROSInterruptException:
+        pass
