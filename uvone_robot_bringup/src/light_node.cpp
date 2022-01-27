614e12c3fbd4a3c8730c05e40d6360fa0604efa0
Rafael Martin <rafael.martin3@alu.uclm.es>
2022-01-27 15:51:40 +0100

    Modified light_node, added led

--- uvone_robot_bringup/src/light_node.cpp
+++ uvone_robot_bringup/src/light_node.cpp
@@ -2,6 +2,7 @@
 #include <kobuki_msgs/DigitalOutput.h>
 #include <kobuki_msgs/SensorState.h>
 #include <kobuki_msgs/Sound.h>
+#include <kobuki_msgs/Led.h>
 
 #include <std_srvs/Empty.h>
 #include <std_srvs/SetBool.h>
@@ -13,13 +14,15 @@ struct LightNode_t {
     ros::ServiceServer _power_off;
     ros::ServiceServer _enable_light;
     ros::ServiceServer _disable_light;
-    ros::Subscriber _sensors;
+    ros::Subscriber _voltage_sensor;
     ros::Publisher _sounds;
+    ros::Publisher _led2;
     ros::Timer _sound_timer;
     uint8_t _sound_selected;
     int32_t _selected_light {-1}; // 0->left  1->right  -1-> none
     bool _power_state;
     bool _inverter_state;
+    bool _reading_state;
 
     LightNode_t(ros::NodeHandle* nh) {
         _digital_output = nh->advertise<kobuki_msgs::DigitalOutput>("/mobile_base/commands/digital_output", 10, true);
@@ -28,37 +31,48 @@ struct LightNode_t {
         _power_off = nh->advertiseService("/select_light_none", &LightNode_t::callback_select_light_none, this);
         _enable_light = nh->advertiseService("/enable_light", &LightNode_t::callback_enable_light, this);
         _disable_light = nh->advertiseService("/disable_light", &LightNode_t::callback_disable_light, this);
-        _sensors = nh->subscribe("/mobile_base/sensors/core", 1, &LightNode_t::get_analog_data, this);
-        _sounds = nh->advertise<kobuki_msgs::Sound>("/mobile_base/commands/sound", 1, false);
+        _voltage_sensor = nh->subscribe("/mobile_base/sensors/core", 1, &LightNode_t::get_analog_data, this);
+        _sounds = nh->advertise<kobuki_msgs::Sound>("/mobile_base/commands/sound", 0, false);
+        _led2 = nh->advertise<kobuki_msgs::Led>("/mobile_base/commands/led2", 0, false);
 
+        // Esperamos a que el nodo se haya suscrito al topic
         while(_digital_output.getNumSubscribers()<1);
 
         set_inverter(false);
         set_light(0);
         set_power(false);
+        set_reading(true);
         _selected_light = -1;
 
-        _sound_timer = nh->createTimer(ros::Rate(2), &LightNode_t::remember_sound, this);
-        _sound_timer.stop();
-
+        _sound_timer = nh->createTimer(ros::Rate(2), &LightNode_t::remember_sound, this, false, false);
+        //_sound_timer.stop();
     }
 
     void get_analog_data(const kobuki_msgs::SensorStateConstPtr& msg) {
-        float voltage = float(msg->analog_input[0])/float(1<<12)*3.3*5.7;
+        static float avg_voltage {float(msg->analog_input[0])/float(1<<12)*3.3f*5.7f};
+        constexpr int N {10};
+        float voltage = float(msg->analog_input[0])/float(1<<12)*3.3f*5.7f;
+        avg_voltage -= avg_voltage/N;
+        avg_voltage += voltage/N;
+        ROS_DEBUG("Voltaje de la bateria: %.2f V", avg_voltage);
 
-        if (_inverter_state && voltage<5.5) {
-            set_inverter(false);
-            ros::Duration(0.01).sleep();
-            set_inverter(true);
-            ros::Duration(0.01).sleep();
+        kobuki_msgs::Led led_msg;
+
+        if (avg_voltage > 16.0f || avg_voltage < 12.0f) {
+            ROS_WARN("Voltaje de la bateria: %.2f V", avg_voltage);
+            led_msg.value = led_msg.RED;
+        } else if (avg_voltage < 14.0f) {
+            ROS_WARN("Voltaje de la bateria: %.2f V", avg_voltage);
+            led_msg.value = led_msg.ORANGE;
+        } else {
+            led_msg.value = led_msg.GREEN;
         }
 
-        ROS_DEBUG("Voltaje de la bateria: %.4f V", voltage);
+        _led2.publish(led_msg);
     }
 
     void set_light(uint32_t selected_light) {
-
-        /* Si la luz ya está seleccionada, no hay nada mas que hacer */
+        /* Si la luz ya está seleccionada, no hay nada que hacer */
         if(_selected_light == selected_light) {
             return;
         }
@@ -66,24 +80,27 @@ struct LightNode_t {
         /* Apagamos el inversor si está encendido, para evitar hacer el cambio con corriente */
         if (_power_state) {
             set_power(false);
-            ros::Duration(0.2).sleep();
+            ros::Duration(0.25).sleep();
         }
 
         /* Actualizamos la variable que almacena que luz ha sido seleccionada */
         _selected_light = selected_light;
 
-        /* Lanzamos el mensaje que actualiza dicha los reles necesarios */
-        update_selected_light();
+        /* Hcemos el cambio de seleccion del circuito de luz */
+        kobuki_msgs::DigitalOutput msg;
+        msg.mask = {true, false, false, false};
+        if (_selected_light == 1)
+            msg.values = {true, false, false, false};
+        else if (_selected_light == 0)
+            msg.values = {false, false, false, false};
+        _digital_output.publish(msg);
         
-        /* Esperamos hasta que se haya hecho el cambio */
-        ros::Duration(0.1).sleep();
-
-        /* Encendemos el balastro */
+        /* Esperamos hasta que se haya hecho el cambio para encender el balastro */
+        ros::Duration(0.25).sleep();
         set_power(true);
     }
 
     bool callback_select_light_right(std_srvs::Empty::Request &req, std_srvs::Empty::ResponseType &res) {
-
         /* Comprobamos si el robot esta preparado para encender las lamparas, si no devolvemos un error en el servicio */
         if (!_inverter_state)
             return false;
@@ -93,8 +110,8 @@ struct LightNode_t {
         set_light(0);
         return true;
     }
-    bool callback_select_light_left(std_srvs::Empty::Request &req, std_srvs::Empty::ResponseType &res) {
 
+    bool callback_select_light_left(std_srvs::Empty::Request &req, std_srvs::Empty::ResponseType &res) {
         /* Comprobamos si el robot esta preparado para encender las lamparas, si no devolvemos un error en el servicio */
         if (!_inverter_state)
             return false;
@@ -104,8 +121,8 @@ struct LightNode_t {
         set_light(1);
         return true;
     }
-    bool callback_select_light_none(std_srvs::Empty::Request &req, std_srvs::Empty::ResponseType &res) {
 
+    bool callback_select_light_none(std_srvs::Empty::Request &req, std_srvs::Empty::ResponseType &res) {
         /* Comprobamos si el robot esta preparado para encender las lamparas, si no devolvemos un error en el servicio */
         if (!_inverter_state)
             return false;
@@ -158,6 +175,11 @@ struct LightNode_t {
         update_inverter();
     }
 
+    void set_reading(bool value) {
+        _reading_state = value;
+        update_reading();
+    }
+
     void update_inverter() {
         kobuki_msgs::DigitalOutput msg;
         msg.mask = {false, true, false, false};
@@ -178,13 +200,13 @@ struct LightNode_t {
         _digital_output.publish(msg);
     }
 
-    void update_selected_light() {
+    void update_reading() {
         kobuki_msgs::DigitalOutput msg;
-        msg.mask = {true, false, false, false};
-        if (_selected_light == 1)
-            msg.values = {true, false, false, false};
-        else if (_selected_light == 0)
+        msg.mask = {false, false, false, true};
+        if (_reading_state == 1)
             msg.values = {false, false, false, false};
+        else if (_reading_state == 0)
+            msg.values = {false, false, false, true};
         _digital_output.publish(msg);
     }
 
@@ -199,8 +221,6 @@ struct LightNode_t {
     }
 
 };
-
-
 
 
 int main(int argc, char** argv) {
