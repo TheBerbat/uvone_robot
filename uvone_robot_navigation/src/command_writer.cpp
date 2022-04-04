b64b0d16b068fef1df0c0a006583e68f205622f5
Rafael Martin <theverbat@gmail.com>
2022-04-04 16:37:08 +0200

    Fixed RPY write cmd file, changed to reparam and fixed launch file

--- uvone_robot_navigation/src/command_writer.cpp
+++ uvone_robot_navigation/src/command_writer.cpp
@@ -3,23 +3,23 @@
 
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseArray.h>
+#include <tf/tf.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 #include <iostream>
 #include <fstream>
 
-std::ofstream file;
-
-
 struct FileWriter
 {
     explicit FileWriter(ros::NodeHandle& nh)
-      : targets_sub_{nh.subscribe<geometry_msgs::PoseStamped>("/move_base_simple/goal", 10, &FileWriter::targets_sub_callback, this)}
+      : targets_sub_{nh.subscribe<geometry_msgs::PoseStamped>("goal", 10, &FileWriter::targets_sub_callback, this)}
       , poses_feedback_pub_{nh.advertise<geometry_msgs::PoseArray>("feedback", 10)}
       , file_cmd_{}
       , poses_{}
       , poses_feedback_timer_{nh.createTimer(ros::Rate{50.0}, &FileWriter::poses_feedback_callback, this)}
     {
         open_file(nh);
+        poses_.header.frame_id="map";
     }
 
     ~FileWriter()
@@ -30,7 +30,18 @@ struct FileWriter
 
     void targets_sub_callback(const geometry_msgs::PoseStampedConstPtr& msg)
     {
-        file_cmd_ << "MOVE " << msg->pose.position.x << " " << msg->pose.position.y << " "<< msg->pose.orientation.z << "\n";
+        tf::Quaternion q{
+            msg->pose.orientation.x,
+            msg->pose.orientation.y,
+            msg->pose.orientation.z,
+            msg->pose.orientation.w,
+        };
+        tf::Matrix3x3 m{q};
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
+        
+
+        file_cmd_ << "MOVE " << std::fixed << std::setprecision(4) << msg->pose.position.x << " " << msg->pose.position.y << " "<< yaw << "\n";
         poses_.poses.push_back(msg->pose);
         poses_.header=msg->header;
     }
@@ -44,14 +55,11 @@ struct FileWriter
     {
         std::string filename_abs {ros::package::getPath("uvone_robot_navigation")+"/paths/default.txt"};
         if(!nh.getParam("path_filename", filename_abs))
-        {
             ROS_WARN("No filename path. Default is: %s", filename_abs.c_str());
-        }
+            
         file_cmd_.open(filename_abs);
         if(!file_cmd_)
-        {
             ROS_ERROR("Cannot open file: %s", filename_abs.c_str());
-        }
     }
 
 private:
@@ -68,12 +76,10 @@ int main(int argc, char** argv)
     ros::NodeHandle nh{"~"};
 
     FileWriter a{nh};
-
     ROS_INFO("Node ready to listen poses to create command file.");
+    ROS_INFO("Press Ctrl+C to write poses file and exit.");
 
-    while(ros::ok())
-        ros::spinOnce();
-
+    ros::spin();
 
     return 0;
 }
